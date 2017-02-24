3c0d8334c8fa1e69d7aa7ca1f71be638838925bb
Taylor <tbaustin@eagles.usi.edu>
2017-02-24 07:02:56 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'tyleraustin',
-    text: 'hey its tyler'
-  });
 });
 
 socket.on('disconnect', function () {
