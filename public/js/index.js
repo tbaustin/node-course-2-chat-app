70b70170ae55fbdee6097fa07d0180ab3425ab73
Taylor <tbaustin@eagles.usi.edu>
2017-02-28 17:34:46 -0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -28,14 +45,9 @@ socket.on('newLocationMessage', function(message) {
     createdAt: formatedTime,
     url: message.url
   });
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
-  //
-  // li.text(`${message.from} - ${formatedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#messageForm').on('submit', function (e) {
