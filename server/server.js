8f69b09e7a1813aed9423677d2ef9aad501fe134
Taylor <tbaustin@eagles.usi.edu>
2017-03-01 09:15:55 -0600

    Added Users class to help with rooms

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -16,9 +17,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('name and room name are required');
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user joined'));
+    socket.join(params.room);
+    //socket.leave('The Office Fans');
+
+    //io.emit -> io.to('The Office Fans').emit
+    // socket.broadcast.emit -> socket.broadcast.to('The Office Fans').emit
+    //socket.emit
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
 
   socket.on('createMessage', (newMessage, callback) => {
     console.log('createMessage', newMessage);
